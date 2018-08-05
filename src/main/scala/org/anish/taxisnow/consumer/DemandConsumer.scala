08e09b91a88c2736d35d68531f713a1766b377e3
Minh Mai <minh.v.mai@gmail.com>
2018-08-05 17:09:14 -0400

    started prototype of streaming data

---
+++ src/main/scala/org/anish/taxisnow/consumer/DemandConsumer.scala
@@ -0,0 +1,31 @@
+package org.anish.taxisnow.consumer
+package stream
+
+import org.apache.spark.sql.SparkSession
+
+// Goal is to read from Kafka topic and write to CSV
+
+class DemandConsumer {
+
+  val spark = SparkSession
+    .builder
+    .appName("DemandConsumerToSpark")
+    .getOrCreate()
+
+  val df = spark.readStream
+    .format("kafka")
+    .option("kafka.bootstrap.servers", "localhost:9092")
+    .option("subscribe", "taxis_demand")
+    .load()
+
+  val query = df.writeStream
+    .format("csv")
+    .option("path", "~/Desktop/kafka-test")
+    .option("checkpointLocation", "~/Desktop/checkpoints")
+
+  // start the streaming
+  while(true) {
+    query.start()
+  }
+
+}
